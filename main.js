74f29f9a7136872af6aa8da146d4a6dcc8dcafc8
Laibakamrankhan <160596709+Laibakamrankhan@users.noreply.github.com>
2024-05-10 01:40:54 +0500

    Rock Paper Scissor Game

---
+++ main.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+let answer1 = await inquirer.prompt({
+    name: "player1",
+    type: "list",
+    message: "Chose 1 Option",
+    choices: ["Rock", "Paper", "Scissor"]
+});
+let answer2 = await inquirer.prompt({
+    name: "player2",
+    type: "list",
+    message: "Chose 1 Option",
+    choices: ["Rock", "Paper", "Scissor"]
+});
+if (answer1.player1 == answer2.player2) {
+    console.log("Game tie");
+}
+else if ((answer1.player1 == "Rock" && answer2.player2 == "Paper") || (answer1.player1 == "Scissor" && answer2.player2 == "Paper") ||
+    (answer1.player1 == "Rock" && answer2.player2 == "Scissor")) {
+    console.log("Player 1 win");
+}
+else {
+    console.log("Player 2 win");
+}
